86ae709ef314a4487d02f6522098d3444c5053bd
Marc Jermaine Pontiveros <mcpontiveros@up.edu.ph>
2020-11-13 06:51:08 +0800

    Sample CRUD in Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";  
+import logo from "./logo.svg";  
+import "./App.css";  
+import PropTypes from 'prop-types';  
+import { getEmployee, addEmployee, editEmployee, deleteEmployee } from './Redux/actions';  
+import { connect } from 'react-redux';  
+  
+const mapStateToProps = state => ({  
+  employees: state.employees  
+});  
+  
+class App extends Component {  
+  constructor(props) {  
+    super(props);
+    this.state = {
+      id: 0, 
+      employeeName: "",
+      employeeDepartment: "",
+    }  
+  }  
+  
+  static propTypes = {  
+    employees: PropTypes.array.isRequired,  
+    getEmployee: PropTypes.func.isRequired,
+    addEmployee: PropTypes.func.isRequired,
+    editEmployee: PropTypes.func.isRequired,
+    deleteEmployee: PropTypes.func.isRequired  
+  };  
+  
+  componentDidMount() {  
+    this.props.getEmployee();  
+  }  
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  submitData = () => {
+    if(this.state.employeeName && this.state.employeeDepartment && !this.state.id){
+      const newEmployee = {
+        id: Math.floor(Math.random() * (999 - 100 + 1) + 100),
+        employeeName: this.state.employeeName,
+        employeeDepartment: this.state.employeeDepartment,
+      };
+      this.props.addEmployee(newEmployee);
+    } else if(this.state.employeeName && this.state.employeeDepartment && this.state.id){
+      const updatedDetails = {
+        id: this.state.id,  
+        employeeName: this.state.employeeName,  
+        employeeDepartment: this.state.employeeDepartment,
+      };
+      this.props.editEmployee(updatedDetails);
+    } else {
+      alert('Enter employee details!');
+    }
+    this.clearData();
+  }
 
-export default App;
+  clearData = () => {  
+    this.setState({  
+      id: 0,  
+      employeeName: "",  
+      employeeDepartment: ""  
+    });  
+  }  
+
+  editDetails = (data) => {  
+    this.setState({  
+      id: data.id,  
+      employeeName: data.employeeName,  
+      employeeDepartment: data.employeeDepartment  
+    })  
+  }  
+
+  deleteEmployee = (id) => {  
+    this.clearData();  
+    if (window.confirm("Are you sure?")) {  
+      this.props.deleteEmployee(id);  
+    }  
+  }  
+
+  handleNameChange = (e) => {  
+    this.setState({  
+      employeeName: e.target.value  
+    });  
+  }  
+  
+  handleDepartmentChange = (e) => {  
+    this.setState({  
+      employeeDepartment: e.target.value  
+    });  
+  }  
+
+
+  render() {  
+    return (  
+      <div className="App">  
+        <header className="App-header">  
+          <img src={logo} className="App-logo" alt="logo" />  
+          <h1 className="App-title">CRUD opeartions for Employee Module</h1>  
+        </header>  
+        <p className="App-intro">  
+          <div>
+            Employee Name: <input onChange={this.handleNameChange} value={this.state.employeeName} type="text" placeholder="Employee Name" /><br />
+            Employee Department :  <input onChange={this.handleDepartmentChange} value={this.state.employeeDepartment} type="text" placeholder="Employee Department" /><br />  
+            {this.state.id ? <button onClick={this.submitData}>UPDATE</button> : <button onClick={this.submitData}>ADD</button>}   <button onClick={this.clearData}>CLEAR</button>  
+          </div>
+          <div className="rightsection">  
+            <table>  
+              <thead>  
+                <tr>  
+                  <th>ID</th>  
+                  <th>Name</th>  
+                  <th>Depatment Name</th>  
+                  <th>Action(s)</th>  
+                </tr>  
+              </thead>  
+              <tbody>  
+                {this.props.employees && this.props.employees.map((data, index) => {  
+                  return <tr key={(index + 1)}>  
+                    <td>{(index + 1)}</td>  
+                    <td>{data.employeeName}</td>  
+                    <td>{data.employeeDepartment}</td>  
+                    <td><button onClick={() => this.editDetails(data)}>EDIT</button> <button onClick={() => this.deleteEmployee(data.id)}>DELETE</button> </td>  
+                  </tr>  
+                })}  
+              </tbody>  
+            </table>  
+          </div>  
+        </p>  
+      </div>  
+    );  
+  }  
+  
+}  
+
+
+export default connect(mapStateToProps, { getEmployee, addEmployee, editEmployee, deleteEmployee})(App); 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './Redux/reducer';
+
+// Create a store
+//const store = createStore(rootReducer);
+const store = createStore(reducer, applyMiddleware(thunk));
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
